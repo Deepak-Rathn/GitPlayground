ba14e735268b9c161a6ee7679859acfed2ec833f
Kavia AI <production@kavia.ai>
2025-09-21 16:50:15 +0000

    CheckPoint - cm881346f5

--- sample.c
+++ sample.c
@@ -2,6 +2,9 @@
 
 int main (char ac, char *av[])
 {
-
+    // Existing output
 	printf("Hello world Hello Git Hello Github\n");
+    // New requested message
+    printf("This is a sample C program to explore KAVIA AI.\n");
+    return 0;
 }
